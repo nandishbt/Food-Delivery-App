35152e340cf9d7fa95e0cae6f022589a148f8f76
nandishbt <nandishbt2001@gmail.com>
2024-03-22 18:25:20 +0530

    home page created

---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+      <div className="header-content">
+        <h2>Order your favourite food here </h2>
+        <p>Food is nourishment for body and soul, a delightful fusion of flavors, textures,
+           and aromas that brings people together, evoking memories and igniting passions for culinary exploration and enjoyment</p>
+        <button>view menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
